916b02eab57d585b121a9077b664958ec24c7db2
Sue Tarazi <suemachtley@gmail.com>
2020-03-14 16:21:46 -0700

    First draft of Challenge4 - trouble with generating random array; can't traverse array with dummy data yet.

--- Prework-401-CodeChallenges/Program.cs
+++ Prework-401-CodeChallenges/Program.cs
@@ -7,6 +7,7 @@ namespace Prework_401_CodeChallenges
         static void Main(string[] args)
         {
             CalculateLeapyear();
+            Challenge4();
         }
 
         public static void Challenge1()
@@ -39,6 +40,37 @@ namespace Prework_401_CodeChallenges
 
         }
 
+    public static void Challenge4()
+        {
+            //user input for length and width of matrix
+            Console.WriteLine("Please enter the number of rows and columns for your array   ");
 
+            //create 2d array and populate with random numbers
+            /*
+            int[,] multiDimensionalArray1 = new int[rows, columns];
+            Random rand = new Random();
+            for (i=0; i<rows; i++)
+            {
+                for(j=0; j<columns; j++)
+                {
+                    twoDimArray[i] = rand.Next(0, 9);
+                }
+            }
+            */
+            //making a test matrix with sample data shown in challenge4 info
+            int[,] myArray = new int[3, 5] { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 } };
+
+            //nested for loop to add each row
+            int sum = 0;
+            for(var i=0; i<myArray.Length; i++)
+            {
+                for (var j = 0; j < myArray[i].Length; j++)
+                {
+                    sum += myArray[i][j];
+                }
+            }
+            //return new array
+            Console.WriteLine(myArray);
+        }
     }
 }
